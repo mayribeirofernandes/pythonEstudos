cac91987ce30bec4c320e6399bd09bd43ed3df16
May Fernandes <ammmayara@hotmail.com>
2019-01-21 13:01:52 -0200

    check to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -17,18 +19,35 @@ class NewVisitorTest(unittest.TestCase):
 
         # Ela percebe que o título da página e o cabeçalho mencionam listas to-do
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!!!!!!!!!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        # self.fail('Finish the test!!!!!!!!!')
 
         # Ela é convidada a inserir um item de tarefa imediamente
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # Ela digita "Buy peacock feathears" (comprar penas de pavão) em uma caixa
         # de texto (o hooby de Edith é fazer iscas para pesca com fly)
+        inputbox.send_keys('Buy peacock feathers')
 
         # Quando ela tecla enter a página é atualizada e agora a página lista
         # "1: Buy peacock feathears" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
         # item. Ela insere "use peacock feathears to make a fly" (Usar penas de pavão
         # para fazer um fly - Edith é bem metódica)
+        self.fail('Finish this!!!!')
 
         # A página é atualizada novamente e agora mostra os 2 itens da sua listas
 
