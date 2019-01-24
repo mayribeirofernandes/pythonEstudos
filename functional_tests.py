20a2e87881276bb2ca7687111104a9dfae85c0fe
May Fernandes <ammmayara@hotmail.com>
2019-01-24 12:54:24 -0200

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,8 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        # self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
@@ -41,7 +42,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
