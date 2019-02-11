34016ae7bbf7b7067e69c194c9e21a7c291546c0
May Fernandes <ammmayara@hotmail.com>
2019-02-11 13:01:04 -0200

    Save item

--- lists/tests.py
+++ lists/tests.py
@@ -26,8 +26,17 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render, redirect
+from lists.models import Item# Create your views here.
 
 
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', '')
-    })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    return render(request, 'home.html')
